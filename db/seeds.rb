48084f4f71017c997aa7ca110f95c7de476efb44
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-10-04 13:40:47 +0330

    Seed the greetings database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Hello, react and rails world!")
+Greeting.create(message: "Hi there, welcom to react and rails!")
+Greeting.create(message: "Greetings from react and rails!")
+Greeting.create(message: "Welcome, to the react and rails introduction!")
+Greeting.create(message: "Good day, react and rails lover!")
